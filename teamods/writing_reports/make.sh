1fcd3f533f26e72410dbc0f65dabd943ab5f8c22
Hans Petter Langtangen <hpl@simula.no>
2013-03-03 17:20:12 +0100

    updates

--- teamods/writing_reports/make.sh
+++ teamods/writing_reports/make.sh
@@ -3,3 +3,4 @@ cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/archived-reports/* .
 
 # update wiki with mediawiki
 cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/archived-reports/_static/report.mwiki ~/vc/hplgit.github.com.wiki/Experiments-with-Schemes-for-Exponential-Decay.mediawiki
+cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/archived-reports/_static/*.png ~/vc/hplgit.github.com.wiki/
