bbf2d3fc0d77d3d7f8a7b6cacc4034f993ff65e7
Hans Petter Langtangen <hpl@simula.no>
2013-03-03 16:48:08 +0100

    updates

--- teamods/writing_reports/make.sh
+++ teamods/writing_reports/make.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/reports/* .
+cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/archived-reports/* .
+
+# update wiki with mediawiki
+cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/archived-reports/_static/report.mwiki ~/vc/hplgit.github.com.wiki/Experiments-with-Schemes-for-Exponential-Decay.mediawiki
--- teamods/writing_reports/project_doconce/doc/run.sh
+++ teamods/writing_reports/project_doconce/doc/run.sh
@@ -7,15 +7,15 @@ python decay_exper1_do.py 1.25 0.75 0.5 0.1
 
 # HTML
 file=tmp_report
-doconce format html report_mathjax.html
-mv report_mathjax.html.html report_do.html
+doconce format html report_md.html
+mv report_md.html.html report_do.html
 
 # LaTeX
-doconce format pdflatex report_mathjax.html
-doconce ptex2tex report_mathjax.html envir=minted
-pdflatex -shell-escape report_mathjax.html
-pdflatex -shell-escape report_mathjax.html
-mv report_mathjax.html.pdf report.pdf
+doconce format pdflatex report_md.html
+doconce ptex2tex report_md.html envir=minted
+pdflatex -shell-escape report_md.html
+pdflatex -shell-escape report_md.html
+mv report_md.html.pdf report.pdf
 
 # Sphinx
 doconce sphinx_dir theme=pyramid report
