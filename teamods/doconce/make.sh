6d11f570ff3a28213c843b706b1de86956d28884
Hans Petter Langtangen <hpl@simula.no>
2013-12-29 09:20:42 +0100

    updates

--- teamods/doconce/make.sh
+++ teamods/doconce/make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Copy from doconce/doc/pub/slides
-cp -r ~/vc/doconce/doc/pub/slides/* ~/vc/doconce/doc/pub/slides/._part* .
+cp -r ~/vc/doconce/doc/pub/slides/* ~/vc/doconce/doc/pub/slides/._* .
 git add .
