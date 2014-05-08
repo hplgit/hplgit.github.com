d442fd744a3968448512fb678f8b41c7c4985b0f
Hans Petter Langtangen <hpl@simula.no>
2014-05-08 17:08:17 +0200

    New updates (copies)

---
+++ teamods/debugging/make.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copy documents from development repo
+name=debug
+wrap=main_${name}
+repo=$HOME/vc/primer4/doc/src/chapters/debug
+
+rm -rf html *.html *.pdf
+#cp -r $repo/sphinx-rootdir/_build/html .
+#cp -r $repo/sphinx-rootdir-extended/_build/html html-githg
+cp $repo/$wrap.pdf $name.pdf
+#cp $repo/${wrap}_4print.pdf ${name}_4print.pdf
+cp $repo/${wrap}.html $repo/._${wrap}*.html .
+#cp $repo/${wrap}-solarized.html ${name}-solarized.html
+git add .
+git commit -am 'New updates (copies)'
