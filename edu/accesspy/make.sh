2f501be7ad549bc089ce61c940c3e28b963dd0f8
Hans Petter Langtangen <hpl@simula.no>
2014-06-19 14:24:55 +0200

    updates

--- edu/accesspy/make.sh
+++ edu/accesspy/make.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 dir=~/vc/primer4/doc/src/chapters/tech
-cp -r $dir/accesspy.html $dir/._accesspy*.html $dir/fig-accesspy .
+cp -r $dir/accesspy*.html $dir/._accesspy*.html $dir/fig-accesspy .
+rm -rf accesspy_blog*
