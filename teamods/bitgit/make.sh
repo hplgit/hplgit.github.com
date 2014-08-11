b32f21458776e1e63cd5900c8b2af8a2609266a8
Hans Petter Langtangen <hpl@simula.no>
2014-08-11 23:32:21 +0200

    New updates (copies)

--- teamods/bitgit/make.sh
+++ teamods/bitgit/make.sh
@@ -7,10 +7,15 @@ repo=$HOME/vc/teamods/doc/src/$name
 rm -rf html *.html .*.html *.pdf
 cp -r $repo/sphinx-rootdir/_build/html .
 cp -r $repo/sphinx-rootdir-extended/_build/html html-githg
+cp -r $repo/sphinx-rootdir-github/_build/html html-github
 cp $repo/${wrap}*.pdf .
+cp $repo/main_github*.pdf .
 cp $repo/${wrap}*.html $repo/._${wrap}*.html .
+cp $repo/main_github*.html $repo/._main_github*.html .
 scitools rename $wrap Langtangen_$name *.pdf *.html .*.html
+scitools rename main_github Langtangen_github *.pdf *.html .*.html
 scitools replace $wrap Langtangen_$name *.html .*.html
+scitools replace main_github Langtangen_github *.html .*.html
 rm -f *~ .*~
 git add .
 git commit -am 'New updates (copies)'
