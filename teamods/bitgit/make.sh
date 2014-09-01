c869155529741765f099584c098358369df4838e
Hans Petter Langtangen <hpl@simula.no>
2014-09-01 07:27:04 +0200

    New updates (copies)

--- teamods/bitgit/make.sh
+++ teamods/bitgit/make.sh
@@ -4,7 +4,7 @@ name=bitgit
 wrap=main_${name}
 repo=$HOME/vc/teamods/doc/src/$name
 
-rm -rf html *.html .*.html *.pdf
+rm -rf html* *.html .*.html *.pdf
 cp -r $repo/sphinx-rootdir/_build/html .
 cp -r $repo/sphinx-rootdir-extended/_build/html html-githg
 cp -r $repo/sphinx-github/_build/html html-github
