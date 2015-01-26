507bcbdd08c3b25f609e273beee3e383b684dd7f
Hans Petter Langtangen <hpl@simula.no>
2015-01-26 01:03:25 +0100

    updates

---
+++ misc/talks/mn_strategy_2015/make.sh
@@ -0,0 +1,4 @@
+name=mn_strategy
+doconce format html $name
+doconce replace '<title>Forskning _OG_ undervisning</title>' '<title>Forskning OG undervisning</title>' $name.html
+doconce slides_html $name.html reveal --html_slide_theme=beige --html_footer_logo=uio_symbol
