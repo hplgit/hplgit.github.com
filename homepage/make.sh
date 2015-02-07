ee8571795a37fc3eb47b2f35e3a215a0096968c8
Hans Petter Langtangen <hpl@simula.no>
2015-02-07 02:11:46 -0800

    updates

--- homepage/make.sh
+++ homepage/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files="index bio research education software contact books"
+files="index bio research education software contact books book_comparison"
 for file in $files; do
 doconce format html $file --html_style=solarized --html_template=template_solarized_box_yellow.html
 done
