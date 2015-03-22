3d31dd4f46266b7974a0cad0d4120a9303925322
Hans Petter Langtangen <hpl@simula.no>
2015-03-21 22:29:05 -0700

    updates

--- homepage/make.sh
+++ homepage/make.sh
@@ -5,3 +5,5 @@ doconce format html $file --html_style=solarized --html_template=template_solari
 done
 #exit
 doconce subst -s '<!--\s+<footer>.+?</footer>\s+-->' '<footer>Web pages generated from <a href="http://hplgit.github.io/doconce/doc/web/index.html">DocOnce</a> text using the <a href="http://ethanschoonover.com/solarized">solarized color style</a>.</footer>' index.html
+
+rm -f .*_collection *~
