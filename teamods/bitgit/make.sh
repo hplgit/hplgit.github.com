09eac5603fb044af9384afe0c2176c418b376c53
Hans Petter Langtangen <hpl@simula.no>
2015-08-16 00:32:10 -0700

    updates

--- teamods/bitgit/make.sh
+++ teamods/bitgit/make.sh
@@ -6,7 +6,7 @@ repo=$HOME/vc/teamods/doc/src/$name
 
 rm -rf html* *.html .*.html *.pdf
 cp -r $repo/sphinx-rootdir/_build/html .
-cp -r $repo/sphinx-rootdir-extended/_build/html html-githg
+#cp -r $repo/sphinx-rootdir-extended/_build/html html-githg
 cp -r $repo/sphinx-github/_build/html html-github
 cp $repo/${wrap}*.pdf .
 cp $repo/main_github*.pdf .
